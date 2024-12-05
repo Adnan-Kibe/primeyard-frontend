5420fe0ae1a5381b1d6a9848eec82de1a5eef14d
AdnanK1 <adnangitonga@gmail.com>
2024-12-05 16:43:37 +0300

    Pushing to repo

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type AuthLayoutProps = {
+    children: React.ReactNode
+}
+
+const AuthLayout = ({children}: AuthLayoutProps) => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        {children}
+    </div>
+  )
+}
+
+export default AuthLayout
---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+const page = () => {
+    const {  } = useForm()
+
+    return (
+        <div>Sign In page</div>
+    )
+}
+
+export default page
