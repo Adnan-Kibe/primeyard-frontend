f58b3f4f6c9a79d6d3fa927bfa89462b9d509e6c
AdnanK1 <adnangitonga@gmail.com>
2024-12-11 15:54:26 +0300

    Updated the props

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,18 +7,32 @@ export const signInSchema = z.object({
 
 export type TSsignInSchema = z.infer<typeof signInSchema>
 
+export type Image = {
+    image_id: string;
+    image: string;
+};
+
+export type Inquire = {
+    inquire_id: string;
+    name: string;
+    email: string;
+    phone: string;
+    message: string;
+};
 
 export type Property = {
-    property_id: string,
-    name: string,
-    address: string,
-    description: string,
-    price: number,
-    bedrooms: number,
-    bathrooms: number,
-    square_feet: number,
-    latitude: number,
-    longitude: number
-    status: string,
-    is_featured: boolean
-}
+    property_id: string;
+    name: string;
+    address: string;
+    description: string;
+    price: number;
+    bedrooms: number;
+    bathrooms: number;
+    square_feet: number;
+    latitude: number;
+    longitude: number;
+    status: string;
+    is_featured: boolean;
+    images: Image[];
+    inquiries: Inquire[];
+};
